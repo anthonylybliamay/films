bc2c375946fd724401b7cbe4286c81b1dadce755
anthonyanthony lybliamay <anthonyanthony.lybliamay@gelti.fr>
2026-06-19 09:49:29 +0200

    test

---
+++ app/signin/page.tsx
@@ -0,0 +1,6 @@
+NEXT_PUBLIC_SUPABASE_URL=https://urjsnohuvbvogxtnqgcl.supabase.co
+NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_uYZjpfbf5qZOQP56myXt0A_NNQcvMuf
+
+DATABASE_URL="postgresql://postgres.urjsnohuvbvogxtnqgcl:Minji2004%3FAnthony@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?pgbouncer=true"
+
+DIRECT_URL="postgresql://postgres.urjsnohuvbvogxtnqgcl:Minji2004%3FAnthony@aws-0-eu-west-1.pooler.supabase.com:5432/postgres"
